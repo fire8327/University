5edacff9e38eff4c917ab9d422b7edfe300d5220
fire8327 <fire83274@gmail.com>
2023-03-26 21:11:30 +0300

    fix

--- Assets/JS/custom.js
+++ Assets/JS/custom.js
@@ -1,25 +1,6 @@
-/* navigation */
 /*jshint esnext: true */
-const toggler = document.getElementById("toggler");
-const mNav = document.getElementById("mobile__nav");
-const close = document.getElementById("toggler__expanded");
-const overlay = document.getElementById("overlay");
-const body = document.getElementsByTagName("body")[0];
+const menu = document.getElementById("menu");
 
-toggler.addEventListener("click", () => {
-  mNav.classList.remove("translate-x-full");
-  overlay.classList.remove("hidden");
-  body.classList.add("overflow-hidden");
-});
-
-close.addEventListener("click", () => {
-  mNav.classList.add("translate-x-full");
-  overlay.classList.add("hidden");
-  body.classList.remove("overflow-hidden"); 
-});
-
-overlay.addEventListener("click", () => {
-  mNav.classList.add("translate-x-full");
-  overlay.classList.add("hidden"); 
-  body.classList.remove("overflow-hidden"); 
+menu.addEventListener("click", () => {
+  menu.classList.toggle("opacity-70");
 });
